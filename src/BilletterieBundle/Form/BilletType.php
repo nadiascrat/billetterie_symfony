7932f7ec32897552cdf4f9e55a1be12af3480792
nadia <nadia.tronche@gmail.com>
2016-06-08 13:59:27 +0200

    Refonte manuelle du prototype du formulaire billet

--- src/BilletterieBundle/Form/BilletType.php
+++ src/BilletterieBundle/Form/BilletType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class BilletType extends AbstractType
@@ -23,21 +24,31 @@ class BilletType extends AbstractType
         $builder
             ->add('nom', TextType::class)
             ->add('prenom', TextType::class)
-            ->add('nationalite', TextType::class)
+            ->add('nationalite', ChoiceType::class, [
+                      'choices' => array(
+                          'France'=>'France',
+                          'Afrique'=>'Afrique',
+                          'Amérique du Nord'=>'Amérique du Nord',
+                          'Amérique du Sud'=>'Amérique du Sud',
+                          'Asie'=>'Asie',
+                          'Europe (hors France)'=>'Europe',
+                          'Eurasie'=>'Eurasie'                      
+                      )
+                  ])
             ->add('dateNaissance', BirthdayType::class, [
-                  'format' => 'dd MMM yyyy',
-                  'widget' => 'choice',
-                  'attr' => array(
-                      'class' => 'dateNaissance'
-                  )
-              ])
-            ->add('tarifSpecial', CheckboxType::class, array(
+                      'format' => 'dd MMM yyyy',
+                      'widget' => 'choice',
+                      'attr' => array(
+                          'class' => 'dateNaissance'
+                      )
+                  ])
+            ->add('tarifSpecial', CheckboxType::class, [
                       'label'    => "Cochez la case suivante pour bénéficier du tarif spécial (étudiant, employé du musée, d’un service du Ministère de la Culture, militaire) ",
                       'required' => false,
                       'attr' => array(
                       'class' => 'tarifSpecial'
-                  )
-                  ));
+                    )
+                  ]);
         ;
     }
     
